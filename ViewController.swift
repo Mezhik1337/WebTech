30a25322f08ef41a0b74d5a554d7d6a66e393ce7
Mezhik1337 <59055036+Mezhik1337@users.noreply.github.com>
2022-09-30 22:46:35 +0300

    Create ViewController.swift

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessANumberGame
+//
+//  Created by Yura Mezhik on 08.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
